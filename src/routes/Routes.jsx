13ae6ca0c6f417de92286ca241aeb1a105dba658
z3san <ahtisamhaque@gmail.com>
2023-08-24 10:39:51 +0600

    login and signup page added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,30 @@
+
 import { createBrowserRouter } from 'react-router-dom'
 import Main from '../layouts/Main'
+import Home from '../pages/Home/Home'
+import Login from '../pages/Login/Login'
+import SignUp from '../pages/SignUp/SignUp'
+
+
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Main />,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+     } 
+    ]
+    
   },
+  {
+    path: '/login',
+    element: <Login/>
+  },
+  {
+    path: '/signup',
+    element: <SignUp/>
+  }
 ])
